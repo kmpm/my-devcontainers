a442ec721a7a3b7bc0548f6945d21f1cbcbacd57
Peter Magnusson <me@kmpm.se>
2025-01-15 16:52:35 +0000

    fix: add golangci-lint to gotools

--- src/go-nats/.devcontainer/local-features/gotools/install.sh
+++ src/go-nats/.devcontainer/local-features/gotools/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 su ${_REMOTE_USER} -c 'go install golang.org/x/tools/cmd/goimports@latest'
-
+su ${_REMOTE_USER} -c 'curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin'
 
